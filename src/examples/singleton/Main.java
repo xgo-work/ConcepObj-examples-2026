d56bad3fec30a469524e82ad6b02285569904679
xgo-work <71324808+xgo-work@users.noreply.github.com>
2026-04-30 10:17:23 +0200

    completed singleton main example

--- src/examples/singleton/Main.java
+++ src/examples/singleton/Main.java
@@ -18,6 +18,13 @@ public class Main {
         //System.out.println(game3);
         //System.out.println(game4);
 
+        /*
+        if(game3 == game4){
+            System.out.println("Les deux instances sont identiques! Implémentation du singleton réussie");
+        } else {
+            System.out.println("!!! Les deux instances sont différentes! Il y a une erreur dans la mise en place du singleton");
+        }
+        */
         // Verification : les deux instances doivent être identiques, c'est à dire que les deux références pointent vers le même objet.
         // On le voit grâce à l'adresse mémoire affichée dans la console, qui doit être la même pour les deux références.
 
